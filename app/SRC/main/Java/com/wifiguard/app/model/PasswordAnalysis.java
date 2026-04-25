5c06ffc1e422b778356fed562f2af4d5d32884a0
Enayatullah Haqqani <zadkiel9067@gmail.com>
2026-04-25 16:24:37 +0430

    Add PasswordAnalysis class for password strength analysis

--- app/SRC/main/Java/com/wifiguard/app/model/PasswordAnalysis.java
+++ app/SRC/main/Java/com/wifiguard/app/model/PasswordAnalysis.java
@@ -1,1 +1,16 @@
+package com.wifiguard.app.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class PasswordAnalysis {
+    public int strengthScore;     // 0..100
+    public String strengthLabel;  // "亘爻蹖丕乇 囟毓蹖賮", ...
+    public int strengthColor;     // ARGB
+    public double entropyBits;
+    public int length;
+    public boolean hasLower, hasUpper, hasDigit, hasSymbol;
+    public boolean isCommon;
+    public String estimatedCrackTime;
+    public List<String> suggestions = new ArrayList<>();
+}
